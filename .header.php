8aa9c7378ef953243da1cc802c4f12dc918f6ec4
3004Kristina <kristina.usova@gmail.com>
2020-12-27 14:49:23 +0300

    add section main banner

--- .header.php
+++ .header.php
@@ -13,12 +13,35 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remodal/1.1.1/remodal.min.css" integrity="sha512-UO+dUiFTr6cCaPZKCzXEGhYsuK8DkGAS5iThyMUrtHsg+INCFyRM3GiqJ4rjuvfEyn81XGjpfmjSwwR1dAjAsw==" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.6.3/nouislider.css" integrity="sha512-XXtRBFtk/QfR8GEWwQPYjrQBHQwjidXg0wo8HJi9YOaFycWqd2uWkjJoAyx8Mb/+H8uhvmf70EAIxDnQxrwrvw==" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remodal/1.1.1/remodal-default-theme.min.css" integrity="sha512-jRxwiuoe3nt8lMSnOzNEuQ7ckDrLl31dwVYFWS6jklXQ6Nzl7b05rrWF9gjSxgOow5nFerdoN6CBB4gY5m5nDw==" crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/fonts/stylesheet.css">
 </head>
 <body>
 <!--HEADER START-->
 <header class="header_wrapper">
-
+    <div class="container">
+        <div class="main_menu_wrapper">
+            <div class="main_menu">
+                <ul class="menu left_col">
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">TEAM</a></li>
+                    <li><a href="#">BOOKING</a></li>
+                </ul>
+                <div class="img_wrapper">
+                    <img src="/img/Vector.png" alt="menu-image">
+                </div>
+                <ul class="menu right_col">
+                    <li><a href="#">MENU</a></li>
+                    <li><a href="#">GALERIE</a></li>
+                    <li><a href="#">EVENTS</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </header>
 <!--HEADER END-->
 
--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php require __DIR__ . '/.header.php'; ?>
 
-
+<!--SECTION MAIN BANNER START-->
+<section class="main_banner_wrapper">
+    <div class="container">
+        <div class="main_banner_content">
+            <h4>RESTAURANT</h4>
+            <h1>hungry people</h1>
+            <div class="btn_wrapper">
+                <button class="btn">BOOK TABLE</button>
+                <button class="btn white">EXPLORE</button>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN BANNER END-->
 
 <?php require __DIR__ . '/.footer.php'; ?>
